ba05ffcc7eec9ed7580acbd83dd0a77aeb8b42eb
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-05-14 22:58:25 +0300

    Added Reed-Solomon encoding and logs

--- nucleo/src/main.rs
+++ nucleo/src/main.rs
@@ -8,11 +8,21 @@ use panic_halt as _;
 use stm32f7xx_hal as hal;
 
 use crate::hal::{adc::Adc, delay::Delay, pac, prelude::*};
+use core::fmt::Write;
 use cortex_m_rt::entry;
 use micromath::F32Ext;
-use rtt_target::{rtt_init};
+use reed_solomon::Decoder;
+use reed_solomon::Encoder;
+use rtt_target::rtt_init;
 use tinyvec::ArrayVec;
-use core::fmt::Write;
+
+    const SAMPLE_COUNT: usize = 100;
+    const MESSAGE: [u8; 11] = *b"Hello, led!";
+    const INTERVALS: [u32; 13] = [
+        1, 10, 100, 200, 300, 400, 500, 1000, 2000, 4000, 8000, 16000, 32000,
+    ];
+    const ECC_LENGTHS: [usize; 6] = [4, 8, 16, 32, 64, 128];
+
 
 #[entry]
 fn main() -> ! {
@@ -48,82 +58,98 @@ fn main() -> ! {
 
     let mut adc = Adc::adc1(adc, &mut apb, clocks, 12, false);
 
-    const sampling_frame: usize = 100;
-    const message: [u8; 11] = *b"Hello, led!";
     green.set_low();
 
-    let intervals: [u32; 15] = [1, 10, 100, 200, 300, 400, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000];
+    let mut high = ArrayVec::<[u16; SAMPLE_COUNT]>::default();
+    let mut low = ArrayVec::<[u16; SAMPLE_COUNT]>::default();
+    let mut received_message = ArrayVec::<[u8; 1000]>::default();
 
-    let mut high = ArrayVec::<[u16; sampling_frame]>::default();
-    let mut low = ArrayVec::<[u16; sampling_frame]>::default();
+    for ecc in ECC_LENGTHS {
+        for interval in INTERVALS {
+            high.clear();
+            low.clear();
+            received_message.clear();
 
-    let mut received_message = ArrayVec::<[u8; 11]>::default();
-    for interval in intervals {
-        high.clear();
-        low.clear();
-        received_message.clear();
-
-        for i in 0..sampling_frame {
-            green.set_low();
-            delay.delay_us(interval);
-
-            let val: u16 = adc.read(&mut a6_in).unwrap();
-            low.push(val);
-
-            green.set_high();
-            delay.delay_us(interval);
-
-            let val: u16 = adc.read(&mut a6_in).unwrap();
-            high.push(val);
-            red.toggle();
-            //write!(&mut channel,"{}", i);
-        }
-
-        let high_mean = high.iter().sum::<u16>() as f32 / high.len() as f32;
-        let high_std = (high
-            .iter()
-            .fold(0.0, |acc, &x| acc + (x as f32 - high_mean).powi(2))
-            / high.len() as f32)
-            .sqrt();
-
-        let low_mean = low.iter().sum::<u16>() as f32 / low.len() as f32;
-        let low_std = (low
-            .iter()
-            .fold(0.0, |acc, &x| acc + (x as f32 - low_mean).powi(2))
-            / low.len() as f32)
-            .sqrt();
-
-        let border = (high_mean + low_mean) as u16 / 2;
-
-        for byte in message {
-            let mut received_byte = 0;
-            for idx in 0..7 {
-                if byte & (1 << idx) != 0 {
-                    green.set_high();
-                } else {
-                    green.set_low();
-                }
-
+            for _ in 0..SAMPLE_COUNT {
+                green.set_low();
                 delay.delay_us(interval);
 
                 let val: u16 = adc.read(&mut a6_in).unwrap();
-                if val > border {
-                    received_byte = received_byte | (1 << idx);
+                low.push(val);
+
+                green.set_high();
+                delay.delay_us(interval);
+
+                let val: u16 = adc.read(&mut a6_in).unwrap();
+                high.push(val);
+                red.toggle();
+            }
+
+            let high_mean = high.iter().sum::<u16>() as f32 / high.len() as f32;
+            let high_std = (high
+                .iter()
+                .fold(0.0, |acc, &x| acc + (x as f32 - high_mean).powi(2))
+                / high.len() as f32)
+                .sqrt();
+
+            let low_mean = low.iter().sum::<u16>() as f32 / low.len() as f32;
+            let low_std = (low
+                .iter()
+                .fold(0.0, |acc, &x| acc + (x as f32 - low_mean).powi(2))
+                / low.len() as f32)
+                .sqrt();
+
+            let border = (high_mean + low_mean) as u16 / 2;
+
+            let enc = Encoder::new(ecc);
+            let dec = Decoder::new(ecc);
+
+            let enc_message = enc.encode(&MESSAGE);
+
+            let enc_message = *enc_message;
+
+            for byte in enc_message.iter() {
+                let mut received_byte = 0;
+                for idx in 0..7 {
+                    if byte & (1 << idx) != 0 {
+                        green.set_high();
+                    } else {
+                        green.set_low();
+                    }
+
+                    delay.delay_us(interval);
+
+                    let val: u16 = adc.read(&mut a6_in).unwrap();
+                    if val > border {
+                        received_byte = received_byte | (1 << idx);
+                    }
+                }
+                received_message.push(received_byte);
+            }
+
+            match dec.correct(&mut received_message, None) {
+                Ok(recovered) => {
+                    write!(
+                            &mut channel,
+                            "interval: {}us, high mean: {} +/- {}, low mean: {} +/- {}, ecc len: {} message: ",
+                            interval, high_mean, high_std, low_mean, low_std, ecc,
+                        )
+                            .unwrap();
+                    channel.write(&recovered.data());
+                    write!(&mut channel, "\n").unwrap();
+                }
+                Err(_) => {
+                    write!(
+                            &mut channel,
+                            "interval: {}us, high mean: {} +/- {}, low mean: {} +/- {}, ecc len: {}, message decoding failed\n",
+                            interval, high_mean, high_std, low_mean, low_std, ecc,
+                        )
+                            .unwrap();
                 }
             }
-            received_message.push(received_byte);
         }
-        write!(&mut channel,
-            "interval: {}us, high mean: {} +/- {}, low mean: {} +/- {}, message: ",
-            interval,
-            high_mean,
-            high_std,
-            low_mean,
-            low_std,
-        );
-        channel.write(&received_message);
-        write!(&mut channel, "\n");
     }
+    green.set_low();
 
     loop {
         red.toggle();
